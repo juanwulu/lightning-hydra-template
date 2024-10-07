3d272ae813ab4fa74018f2f411bdfd530861c461
David Juanwu Lu <juanwu@purdue.edu>
2024-10-06 22:49:29 -0400

    feat(git) Implement the `parse_git_sha` method

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,12 +1,7 @@
 from src.utils.build import build_callbacks, build_loggers
 from src.utils.logging import get_log_level, get_logger, log_hyperparams
 from src.utils.rich import enforce_tags, print_config_tree
-from src.utils.tools import (
-    apply_extras,
-    get_metric_value,
-    parse_git_info,
-    task_wrapper,
-)
+from src.utils.tools import apply_extras, get_metric_value, parse_git_info, task_wrapper
 
 __all__ = [
     "build_callbacks",
